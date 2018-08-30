57ec851a2bd31cbfa84c67378dfb1efc62d25003
Nicolaas <github@sunnysideup.co.nz>
2018-08-30 20:34:08 +1200

    3.0 reversion

--- src/Forms/HTMLEditorFieldShort_Config.php
+++ src/Forms/HTMLEditorFieldShort_Config.php
@@ -1,9 +1,5 @@
 <?php
 
-namespace Sunnysideup\CleanerTinyMCEConfig\Forms;
-
-use SilverStripe\Forms\HTMLEditor\TinyMCEConfig;
-
 
 class HTMLEditorFieldShort_Config implements HTMLEditorFieldShort_ConfigInterface
 {
@@ -14,44 +10,47 @@ class HTMLEditorFieldShort_Config implements HTMLEditorFieldShort_ConfigInterfac
 
     public function setConfig($name = 'cms')
     {
-        TinyMCEConfig::get($name)
-            ->setOption(
-                'valid_styles',
-                array('*' => 'color,font-weight,font-style,text-decoration')
-            )->setOption(
-                'paste_as_text',
-                true
-            )->setOption(
-                'paste_text_sticky',
-                true
-            )->setOption(
-                'paste_text_sticky_default',
-                true
-            )->setOption(
-                'theme_advanced_blockformats',
-                'Paragraph=p;Header 1=h1;Header 2=h2;Header 3=h3;quote=blockquote'
-            )->setButtonsForLine(
-                3,
-                []
-            )->setOption(
-                'width',
-                '702px'
-            )->removeButtons(
-                'outdent',
-                'indent',
-                'numlist',
-                'hr',
-                'pastetext',
-                'pasteword',
-                'visualaid',
-                'anchor',
-                'tablecontrols',
-                'justifyleft',
-                'justifycenter',
-                'justifyright',
-                'strikethrough',
-                'justifyfull',
-                'underline'
-            );
+        HtmlEditorConfig::get('cms')
+        ->setOption(
+            'valid_styles',
+            array('*' => 'color,font-weight,font-style,text-decoration')
+        )->setOption(
+            'paste_as_text',
+            true
+        )->setOption(
+            'paste_text_sticky',
+            true
+        )->setOption(
+            'paste_text_sticky_default',
+            true
+        )->setOption(
+            'theme_advanced_blockformats',
+            'Paragraph=p;Header 1=h1;Header 2=h2;Header 3=h3;quote=blockquote'
+        )->setButtonsForLine(
+            3,
+            []
+        )->removeButtons(
+            'outdent',
+            'indent',
+            'numlist',
+            'hr',
+            'pastetext',
+            'pasteword',
+            'visualaid',
+            'anchor',
+            'tablecontrols',
+            'justifyleft',
+            'justifycenter',
+            'justifyright',
+            'strikethrough',
+            'justifyfull',
+            'underline'
+        );
+
+
+        HtmlEditorConfig::get($name)->setOption(
+            'width',
+            '702px'
+        );
     }
 }
